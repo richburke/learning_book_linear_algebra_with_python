cd02544a6b35c824691bd31a0d0717bbe8e4015b
Rich Burke <rich.burke.dev@gmail.com>
2016-08-14 16:38:54 -0400

    Started some exercises.

---
+++ chapter0-5/01.py
@@ -0,0 +1,11 @@
+
+print('=== CHAPTER 0.5 ===')
+
+print('| Exercise 0.5.1: The number of minutes in a week')
+print(str(7 * 24 * 60) + ' minutes')
+
+print('| Exercise 0.5.2: Find the remainder without using %')
+print(2304811 - (2304811//47) * 47)
+
+print('| Exercise 0.5.3: Is sum divisible by 3?')
+print((673 + 909) % 3 == 0)
